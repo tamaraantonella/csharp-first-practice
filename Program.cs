c74e9cf20cef0fd80277deee7533ecd51aa04cc6
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-07 20:44:17 -0300

    console hello world

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace primer_programa
+{
+    class Program
+    {
+      // programa principal (función principal)
+        static void Main(string[] args)
+        {
+          //instrucciones del programa
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
