3270a029755fabf1a336b3e20849004eb147408d
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 11:13:17 -0300

    ejercicio 4

---
+++ ejercicios-unidad-2/ejercicio4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      float facturado,comision,total;
+      const float porcentaje = 0.05f; // 5% de comisión
+      const int sueldo = 15000;
+      Console.WriteLine("Ingresar total facturado");
+      facturado = float.Parse(Console.ReadLine());
+      comision= facturado * porcentaje;
+      total = sueldo + comision;
+      Console.WriteLine("La comisión es de: " + comision + " y el total a cobrar es de: " + total);
+    }
+    }
+}
