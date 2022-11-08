a11ebf584c880e4158f283b1b4ffc5a8cfeb40d1
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 14:10:12 -0300

    u4 ej 1

--- ejercicios-unidad-4-op-logicos/ejercicio1/Program.cs
+++ ejercicios-unidad-4-op-logicos/ejercicio1/Program.cs
@@ -5,7 +5,18 @@ namespace primer_programa
     {
     static void Main(string[] args)
     {
-
+      // 1. Hacer un programa que solicite el ingreso de dos números y luego calcular:
+      // La resta si el primero es mayor que el segundo.
+      // La suma si son iguales.
+      //  El producto si el primero es menor.
+      // Se deberá emitir un cartel por pantalla con el resultado correspondiente.
+      int a, b;
+      if(a > b)
+        Console.WriteLine(a - b);
+      else if(a < b)
+        Console.WriteLine(a * b);
+      else
+        Console.WriteLine(a + b);
     }
     }
 }
