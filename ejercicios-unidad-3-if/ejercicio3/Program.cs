6b31019f2141fc0ed87b1ecd67c48ca3b4c194df
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 12:20:38 -0300

    u3 ej 3

---
+++ ejercicios-unidad-3-if/ejercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    // programa principal (función principal)
+    static void Main(string[] args)
+    {
+      //3. Una casa de video juegos otorga un descuento dependiendo del importe de la compra realizada según los siguientes valores:
+      // Si el importe es menor a ARS 1000, no hay descuento.
+      // Si el importe es ARS 1000 o más pero menor a ARS 5000, aplica un descuento del 10%.
+      // Si el importe es ARS 5000 o más, aplica un descuento del 18%.
+      //Hacer un programa para ingresar un importe de venta y luego muestre por pantalla el importe final con el descuento que corresponda.
+      float venta,descuento;
+      Console.WriteLine("Ingrese el importe de la venta");
+      venta = float.Parse(Console.ReadLine());
+      if(venta >= 5000)
+        Console.WriteLine("El importe final es " + venta * 0.82 );
+      else if(venta >= 1000 && venta < 5000)
+        Console.WriteLine("El importe final es " +  venta * 0.90);
+      else
+        Console.WriteLine("No hubo descuento, el importe es "+ venta);
+    }
+    }
+}
