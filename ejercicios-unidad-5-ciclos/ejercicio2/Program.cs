2f5a4447b67c4ac5d5b60e2a88905b0f293b836a
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 18:14:32 -0300

    u5 ej 2

---
+++ ejercicios-unidad-5-ciclos/ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //2. Hacer un programa que solicite el ingreso de 10 números
+      //y que muestre el mayor de ellos por pantalla.
+      //Solo se debe emitir UN valor por pantalla.
+      int mayor=0,actual=0;
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine("Ingrese un numero");
+        actual = int.Parse(Console.ReadLine());
+        if(actual > mayor)
+          mayor = actual;
+      }
+
+      Console.WriteLine("El mayor es: " + mayor);
+    }
+    }
+}
