8cea98c04ffe948332ba37c58551514a7f955d09
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 11:25:38 -0300

    guia 2 ejercicio 5

---
+++ ejercicios-unidad-2/ejercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //5. Hacer un programa para ingresar por teclado las tres notas de exámenes de un alumno
+      //y luego calcule y emita por pantalla el promedio final.
+      float nota1, nota2, nota3, promedio;
+      Console.WriteLine("Ingrese la nota del primer parcial");
+      nota1=float.Parse(Console.ReadLine());
+      Console.WriteLine("Ingrese la nota del segundo parcial");
+      nota2=float.Parse(Console.ReadLine());
+      Console.WriteLine("Ingrese la nota del tercer parcial");
+      nota3=float.Parse(Console.ReadLine());
+      promedio = (nota1 + nota2 + nota3) / 3;
+      Console.WriteLine("El promedio de las notas es: " + promedio.ToString("0.00"));
+
+    }
+    }
+}
