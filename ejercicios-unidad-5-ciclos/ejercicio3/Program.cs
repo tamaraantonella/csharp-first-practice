9701c3713ce6bc3e4c19cfa8fa230991420ac20c
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 18:41:51 -0300

    fix

--- ejercicios-unidad-5-ciclos/ejercicio3/Program.cs
+++ ejercicios-unidad-5-ciclos/ejercicio3/Program.cs
@@ -5,43 +5,19 @@ namespace primer_programa
     {
     static void Main(string[] args)
     {
-      //4. Hacer un programa que solicite UN número y luego calcule y emita un cartel aclaratorio si el mismo es primo o no es primo.
-      //Nota: un numero es primo cuando es divisible únicamente por 1 y por sí mismo.
-      int numero;
-      bool primo = false;
-      Console.WriteLine("Ingrese un numero");
-      numero=int.Parse(Console.ReadLine());
-      if(numero > 0 && numero < 4)
-        Console.WriteLine("El numero es primo");
-      for (int i = 2; i < numero; i++)
+      //3. Hacer un programa que solicite 20 edades y luego calcule el promedio de edad de aquellas personas mayores a 18 años.
+      int suma=0,acc=0;
+      for (int i = 0; i < 20; i++)
       {
-        if (numero % i == 0)
+        Console.WriteLine("Ingrese la edad");
+        int edad = int.Parse(Console.ReadLine());
+        if (edad > 18)
         {
-          primo = true;
-          break;
+          acc++;
+          suma += edad;
         }
       }
-      if (primo)
-        Console.WriteLine("El numero no es primo");
-      else
-        Console.WriteLine("El numero es primo");
-
-      //*otra manera de hacerlo
-      //int numero,con=0;
-      //Console.WriteLine("Ingrese un numero");
-      //numero=int.Parse(Console.ReadLine());
-      //for (int i = 2; i < numero; i++)
-      //{
-      //  if (numero % i == 0)
-      //  {
-      //    con++;
-      //  }
-      //}
-      //if (con ===2)
-      //  Console.WriteLine("El numero no es primo");
-      //else
-      //  Console.WriteLine("El numero es primo");
-
+      Console.WriteLine("El promedio de edades es: " + suma/acc);
     }
     }
 }
---
+++ ejercicios-unidad-5-ciclos/ejercicio4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //4. Hacer un programa que solicite UN número y luego calcule y emita un cartel aclaratorio si el mismo es primo o no es primo.
+      //Nota: un numero es primo cuando es divisible únicamente por 1 y por sí mismo.
+      int numero;
+      bool primo = false;
+      Console.WriteLine("Ingrese un numero");
+      numero=int.Parse(Console.ReadLine());
+      if(numero > 0 && numero < 4)
+        Console.WriteLine("El numero es primo");
+      for (int i = 2; i < numero; i++)
+      {
+        if (numero % i == 0)
+        {
+          primo = true;
+          break;
+        }
+      }
+      if (primo)
+        Console.WriteLine("El numero no es primo");
+      else
+        Console.WriteLine("El numero es primo");
+
+      //*otra manera de hacerlo
+      //int numero,con=0;
+      //Console.WriteLine("Ingrese un numero");
+      //numero=int.Parse(Console.ReadLine());
+      //for (int i = 2; i < numero; i++)
+      //{
+      //  if (numero % i == 0)
+      //  {
+      //    con++;
+      //  }
+      //}
+      //if (con ===2)
+      //  Console.WriteLine("El numero no es primo");
+      //else
+      //  Console.WriteLine("El numero es primo");
+
+    }
+    }
+}
