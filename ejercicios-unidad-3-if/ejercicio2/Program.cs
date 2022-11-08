83f048c42d381f119ac55bbec269d6e4c3f62c81
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 11:51:47 -0300

    u3 ejercicio 2

---
+++ ejercicios-unidad-3-if/ejercicio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    // programa principal (función principal)
+    static void Main(string[] args)
+    {
+      //2. Hacer un programa para ingresar un número
+      //y luego se emita un cartel por pantalla “Positivo” si el número es mayor a cero
+      // “Negativo” si el número es menor a cero o
+      //“Cero” si el número es igual a cero.
+      int numero;
+      Console.WriteLine("Ingrese un número");
+      numero = int.Parse(Console.ReadLine());
+      if(numero > 0)
+        Console.WriteLine("Es positivo");
+      else if(numero <0)
+        Console.WriteLine("Es negativo");
+      else
+        Console.WriteLine("Cero");
+    }
+    }
+}
