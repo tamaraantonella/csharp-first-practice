b1068c650d96d645f2a80422111fee88a6e7a772
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 18:38:28 -0300

    u5 ej3

--- ejercicios-unidad-5-ciclos/ejercicio3/Program.cs
+++ ejercicios-unidad-5-ciclos/ejercicio3/Program.cs
@@ -5,19 +5,26 @@ namespace primer_programa
     {
     static void Main(string[] args)
     {
-      //3. Hacer un programa que solicite 20 edades y luego calcule el promedio de edad de aquellas personas mayores a 18 años.
-      int suma=0,acc=0;
-      for (int i = 0; i < 20; i++)
+      //4. Hacer un programa que solicite UN número y luego calcule y emita un cartel aclaratorio si el mismo es primo o no es primo.
+      //Nota: un numero es primo cuando es divisible únicamente por 1 y por sí mismo.
+      int numero;
+      bool primo = false;
+      Console.WriteLine("Ingrese un numero");
+      numero=int.Parse(Console.ReadLine());
+      if(numero > 0 && numero < 4)
+        Console.WriteLine("El numero es primo");
+      for (int i = 2; i < numero; i++)
       {
-        Console.WriteLine("Ingrese la edad");
-        int edad = int.Parse(Console.ReadLine());
-        if (edad > 18)
+        if (numero % i == 0)
         {
-          acc++;
-          suma += edad;
+          primo = true;
+          break;
         }
       }
-      Console.WriteLine("El promedio de edades es: " + suma/acc);
+      if (primo)
+        Console.WriteLine("El numero no es primo");
+      else
+        Console.WriteLine("El numero es primo");
     }
     }
 }
