5d9ebb0a4797f58805004c369515db141fd396bd
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 11:01:50 -0300

    ejercicio 3

---
+++ ejercicios-unidad-2/ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    // programa principal (función principal)
+    static void Main(string[] args)
+    {
+      float km,vel,tiempo;
+      Console.WriteLine("Ingresar km existentes  entre ambas ciudades");
+      km = float.Parse(Console.ReadLine());
+      Console.WriteLine("Ingresar velocidad promedio");
+      vel = float.Parse(Console.ReadLine());
+      tiempo = km/vel;
+      //formateo tiempo para que me muestre asi el resultado y no con muchos decimales
+      Console.WriteLine("El tiempo de viaje es de: " + tiempo.ToString("0.00") + " horas");
+    }
+    }
+}
+
