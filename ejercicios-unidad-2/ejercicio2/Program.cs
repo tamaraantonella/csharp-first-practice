9a6134e058ad1079b8ac94f25e4d5583123b65be
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 10:54:22 -0300

    ejercicio 2

--- ejercicios-unidad-2/ejercicio2/Program.cs
+++ ejercicios-unidad-2/ejercicio2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    // programa principal (función principal)
+    static void Main(string[] args)
+    {
+      int a,cubo;
+      Console.WriteLine("Ingresar un número");
+      a = int.Parse(Console.ReadLine());
+      cubo = a * a * a;
+      Console.WriteLine("el cubo de " + a + " es " + cubo);
+    }
+    }
+}
+
