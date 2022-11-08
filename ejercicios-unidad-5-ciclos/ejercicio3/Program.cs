1da456005a11aadd9653cdf35bf685fc709c2f4e
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-08 18:33:17 -0300

    u5 ej 3

---
+++ ejercicios-unidad-5-ciclos/ejercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //3. Hacer un programa que solicite 20 edades y luego calcule el promedio de edad de aquellas personas mayores a 18 años.
+      int suma=0,acc=0;
+      for (int i = 0; i < 20; i++)
+      {
+        Console.WriteLine("Ingrese la edad");
+        int edad = int.Parse(Console.ReadLine());
+        if (edad > 18)
+        {
+          acc++;
+          suma += edad;
+        }
+      }
+      Console.WriteLine("El promedio de edades es: " + suma/acc);
+    }
+    }
+}
