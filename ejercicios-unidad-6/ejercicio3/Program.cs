e1cdc0ac6717adf6ac2377e7d08bfcbded646cbd
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-09 12:48:08 -0300

    u6 ej 3

---
+++ ejercicios-unidad-6/ejercicio3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //3. Hacer un programa para recibir listas de números positivos que están separadas entre sí por un cero.
+      //El fin de la carga se notifica con un número negativo.
+      //Luego mostrar cuántos números tiene cada lista.
+      int n, total = 0;
+      n = int.Parse(Console.ReadLine());
+      while (n >= 0) {
+        while (n != 0) {
+          total++;
+          n= int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("La lista tiene " + total + " números");
+        total = 0;
+        n = int.Parse(Console.ReadLine());
+      }
+
+    }
+    }
+}
