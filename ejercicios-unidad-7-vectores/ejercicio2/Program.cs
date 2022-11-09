fb2937c6a66e6f1b50419245e2892a84b29307e9
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-09 20:52:24 -0300

    u7 ej2

---
+++ ejercicios-unidad-7-vectores/ejercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //2. Hacer un programa que solicite 10  números enteros y los guarde en un vector.
+      // Luego recorrer ese vector para calcular el promedio.
+      //Mostrar por pantalla los valores que son mayores al promedio.
+      int acc = 0;
+      int promedio;
+      int[] numeros = new int[10];
+      for (int i = 0; i < 10; i++)
+      {
+        Console.WriteLine("Ingrese un numero");
+        numeros[i] = int.Parse(Console.ReadLine());
+        acc+=numeros[i];
+      }
+      promedio = acc/10;
+      for (int i = 0; i < 10; i++)
+      {
+        if (numeros[i] > promedio) {
+          Console.WriteLine("El numero " + numeros[i] + " es mayor al promedio");
+        }
+      }
+    }
+    }
+}
