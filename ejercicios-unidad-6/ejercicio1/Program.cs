1865038a266f6831d785f2c420b925194c463ea8
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-09 12:00:03 -0300

    u6 ej 1

--- ejercicios-unidad-6/ejercicio1/Program.cs
+++ ejercicios-unidad-6/ejercicio1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace primer_programa
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+      //1. Hacer un programa para ingresar 10 números.
+      //El mismo debe analizar y mostrar por pantalla cuántos de esos números son primos.
+      int primos = 0, contador;
+      for (int i = 0; i < 10; i++) {
+        Console.WriteLine("Ingrese un número");
+        int numero = int.Parse(Console.ReadLine());
+        contador = 0;
+        if(numero == 1 || numero == 2)
+          primos++;
+        else{
+          for (int j = 1; j <= numero; j++) {
+            if(numero % j == 0)
+              contador++;
+          }
+          if(contador == 2)
+            primos++;
+        }
+      }
+      Console.WriteLine("La cantidad de números primos ingresados es: " + primos);
+    }
+    }
+}
